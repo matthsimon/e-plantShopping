d9248cdf89f70b53dec468d4d6c82d22cd50bced
Matthieu Simon <matthieu10.simon@laposte.net>
2024-10-04 17:12:37 -0600

    Disable add to cart button after being clicked

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -290,9 +290,9 @@ function ProductList() {
                             <h1 className='cart'>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68">
                                     <g><rect width="156" height="156" fill="none"></rect>
-                                    <circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle>
-                                    <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path>
-                                    <text fill="white" x="130" y="155" font-size="90" text-anchor="middle">{totalCartQuantity}</text></g>
+                                        <circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle>
+                                        <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path>
+                                        <text fill="white" x="130" y="155" font-size="90" text-anchor="middle">{totalCartQuantity}</text></g>
                                 </svg>
                             </h1>
                         </a>
@@ -310,8 +310,12 @@ function ProductList() {
                                     <span className='product-title'>{plant.name}</span>
                                     <p className='product-description'>{plant.description}</p>
                                     <p className='product-price'>{plant.cost}</p>
-                                    <button className={addedToCart[plant.name] ? 'product-button added-to-cart' : 'product-button'}
-                                        onClick={() => handleAddedToCart(plant)}>Add to Cart</button>
+                                    <button
+                                        className={addedToCart[plant.name] ? 'product-button added-to-cart' : 'product-button'}
+                                        disabled={addedToCart[plant.name]}
+                                        onClick={() => handleAddedToCart(plant)}>
+                                        Add to Cart
+                                    </button>
                                 </div>
                             ))}
                         </div>
